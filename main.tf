67a23c7f69776cef7c184297f6cccaeff81f72d5
Hisa Ishibashi <hisa@iporaitech.com>
2025-11-02 21:22:34 -0300

    Use var.project to name the aws_cloudfront_origin_access_control

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ locals {
 }
 
 resource "aws_cloudfront_origin_access_control" "oac" {
-  name                              = "default-oac"
+  name                              = "${var.project}-oac"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
